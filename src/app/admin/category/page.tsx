2b78a3057a433ca247cdc4fa2593f0635083d4fc
AYUMUNISHI <a63yu3m6@gmail.com>
2025-04-25 15:57:47 +0900

    BaaSとの接続

--- src/app/admin/category/page.tsx
+++ src/app/admin/category/page.tsx
@@ -38,13 +38,7 @@ export default function Category() {
     fetcher();
   },[]);
 
-  if (loading) {
-    return <div>読み込み中...</div>
-  }
 
-  if (!posts.length) {
-    return <div>記事がありません</div>
-  }
 
   return (
     <>
@@ -61,7 +55,11 @@ export default function Category() {
             </Link>
           </div>
           <div className="h-[70%] overflow-y-auto border rounded p-2">
-          {
+            { loading ? (
+              <div> 読み込み中...</div>
+            ) :!posts.length ?(
+              <div> 記事がありません</div>
+            ):
             posts.map((post) =>(
               <div 
                 key={post.id}
--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -39,13 +39,7 @@ export default function AdminHome() {
     fetcher();
   }, []);
 
-  if (loading) {
-    return <div>読み込み中...</div>
-  }
 
-  if (!posts.length) {
-    return <div>記事がありません</div>
-  }
 
   return (
     <>
@@ -62,7 +56,11 @@ export default function AdminHome() {
             </Link>
           </div>
           <div className="h-[70%] overflow-y-auto border rounded p-2">
-          {
+          { loading ? (
+              <div> 読み込み中...</div>
+            ) :!posts.length ?(
+              <div> 記事がありません</div>
+            ):
             posts.map((post) => (
               <div key={post.id}
               className="">
